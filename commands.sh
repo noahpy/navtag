fa99f78bdcdc6fc927ac7dd12286af8cb7035532
Noah <ge42joq@mytum.de>
2023-08-11 04:57:58 +0200

    add extra commands

--- commands.sh
+++ commands.sh
@@ -23,3 +23,13 @@ function cpx(){
     cp $(navtag $mfp -t $@)
 }
 
+function mkx(){
+    mkdir $(navtag $mfp -t $@)
+}
+
+function mcx(){
+    results=$(navtag $mfp -t $@)
+    mkdir $results
+    cd $results
+}
+
